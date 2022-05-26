ad2b43f21709a4de510cd279ad5c0ece1359e17e
Maksim <makshuk@bk.ru>
2022-05-26 10:00:16 +0300

    video 2-13

--- script/script.js
+++ script/script.js
@@ -1,1 +1,21 @@
 "use strict"; //строгий режим
+
+const numberOffilms = +prompt("Сколько фильмов вы посмотрели ?", "");
+
+const personalMovieDB = {
+  count: numberOffilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  priver: false,
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+  b = prompt("На сколтко оцените?", ""),
+  c = prompt("Один из последних просмотренных фильмов?", ""),
+  d = prompt("На сколтко оцените?", "");
+
+  personalMovieDB.movies[a] = b;
+  personalMovieDB.movies[c] = d;
+
+  console.log(personalMovieDB);
